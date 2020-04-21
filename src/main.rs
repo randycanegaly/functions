8a04431de05bf967fe5febfebb0debe9237d6e50
randycanegaly <canegaly@gmail.com>
2020-04-21 00:22:07 -0400

    First commit.

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5);
+}
+
+fn another_function(x: i32) {
+    //println!("This is another function.");
+    println!("The value of x is: {0}", x);
+}
